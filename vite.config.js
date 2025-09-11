c2161c844b3b97f9124b7717bf2b0d1756108f9b
ZhongYY96 <1219466560@qq.com>
2025-09-11 23:59:31 +0800

    fix: dep added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // 为 crypto 模块设置别名
+      crypto: 'crypto-browserify',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        // 添加全局变量 polyfill
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          crypto: true,
+        }),
+      ],
+    },
+  },
 })
